086a049c922729b430ae2a4fc8ad058550abc9a0
Adilet Attokurov <64566842+Adilet121286@users.noreply.github.com>
2020-05-08 15:39:09 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -4,6 +4,8 @@ public class LogCall {
     public static void main(String[] args) {
 
         System.out.println("Be happy, all will be nice");
+        
+        System.out.println("Changed version");
 
     }
 }
